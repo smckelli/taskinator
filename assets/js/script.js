1f1c61d01fc3552037c6649156edc35bc37563ac
smckelli <smckelli@gmail.com>
2022-03-19 20:55:28 -0500

    save tasks to local storage 4.4.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,6 +61,8 @@ var createTaskEl = function(taskDataObj) {
   taskDataObj.id = taskIdCounter;
   tasks.push(taskDataObj);
 
+  saveTasks();
+
   // increase task counter for next unique id
   taskIdCounter++;
 };
@@ -118,6 +120,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
       tasks[i].name = taskName;
       tasks[i].type = taskType;
     }
+
+    saveTasks();
   }
 
   alert("Task Updated!");
@@ -168,6 +172,9 @@ var taskStatusChangeHandler = function(event) {
       console.log(tasks);
     }
   }
+
+  saveTasks();
+
 };
 
 var editTask = function(taskId) {
@@ -211,7 +218,13 @@ var deleteTask = function(taskId) {
   }
   // reassign tasks array to be the same as the updatedTasksArr
   tasks = updatedTaskArr;
+
+  saveTasks();
 };
+
+var saveTasks = function() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
